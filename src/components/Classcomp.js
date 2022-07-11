8e7cd8934abca1e4201eb247ceb9eec7d3c2f121
viveksinghkushwah <viveksingh241199@gmail.com>
2022-07-11 15:11:31 +0530

    new

---
+++ src/components/Classcomp.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+
+class Classcomp extends Component{
+    render(){
+        return <h1> class component element</h1>
+    }
+}
+export default Classcomp
---
+++ src/components/Functionalcomp.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import './functionalcomp.css'
+const Functionalcomp =() => <h1 className="Functionalcomp">functional component element</h1>
+export default Functionalcomp
